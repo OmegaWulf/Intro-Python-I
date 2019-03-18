8389fef970e0a3d358d41187845fb6c93338b902
Nikita Thomas <nikcxz@gmail.com>
2019-03-18 15:56:24 -0400

    keyword arguments

--- src/args.py
+++ src/args.py
@@ -7,9 +7,11 @@
 # YOUR CODE HERE
 
 def f1(num1, num2):
-    return num1 + num2
+	return num1 + num2
+
 
 print(f1(1, 2))
+
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
@@ -17,20 +19,24 @@ print(f1(1, 2))
 # YOUR CODE HERE
 
 def f2(*n):
-    total = 0
-    for num in n:
-        total += num
-    return total
+	total: int = 0
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
+	for num in n:
+		total += num
+
+
+	return total
+
+
+print(f2(1))  # Should print 1
+print(f2(1, 3))  # Should print 4
+print(f2(1, 4, -12))  # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(a))  # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -38,9 +44,11 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+def f3(ar, ar2 = 1):
+	return ar + ar2
 
+print(f3(1, 2))  # Should print 3
+print(f3(8))  # Should print 9
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
@@ -51,6 +59,10 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+
+def f4(*, a, b):
+	print(f"key: a, value: {a}")
+	print(f"key: b, value: {b}")
 
 # Should print
 # key: a, value: 12
@@ -64,8 +76,8 @@ f4(a=12, b=30)
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
 d = {
-    "monster": "goblin",
-    "hp": 3
+	"monster": "goblin",
+	"hp": 3
 }
 
 # What thing do you have to add to make this work?
